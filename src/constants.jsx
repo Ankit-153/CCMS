52f858e25c6ee12a326f670bf8538fb492407345
Ankit-153 <ankit.g0153@gmail.com>
2025-03-30 14:54:48 +0530

    Update constants.jsx

--- src/constants.jsx
+++ src/constants.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 
 const API_URL = window.location.hostname === 'localhost' 
   ? 'http://localhost:3000' 
-  : 'http://192.168.241.93:3000';
+  : 'https://ccms-backend-d8r3.onrender.com';
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
